38d54a900773f6ef9fa39a3b5d9962b57dbf768b
Emiliortega7 <128631724+Emiliortega7@users.noreply.github.com>
2024-08-28 13:17:55 +0300

    change variable names

--- auth.py
+++ auth.py
@@ -7,67 +7,67 @@ from flask_jwt_extended import JWTManager, create_access_token, create_refresh_t
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable Flask-SQLAlchemy event system
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
 
 db = SQLAlchemy(app)
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=db.engine))
-jwt = JWTManager(app)
+database_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=db.engine))
+jwt_manager = JWTManager(app)
 
 class User(db.Model):
-    __tablename__ = 'users'  # Explicit table name definition
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
 
-    def set_password(self, password):
+    def set_password_hash(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 @app.before_first_request
-def create_tables():
+def initialize_database():
     db.create_all()
 
 @app.teardown_appcontext
-def remove_session(exception=None):
-    db_session.remove()  # Make sure the session is removed at the end of the request or application context
+def cleanup_database_session(exception=None):
+    database_session.remove()
 
 @app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+def user_registration():
+    user_data = request.get_json()
+    username = user_data.get('username')
+    password = user_data.get('password')
     if not username or not password:
-        return jsonify({"msg": "Username and password are required"}), 400
-    user = User.query.filter_by(username=username).first()
-    if user:
-        return jsonify({"msg": "Username already exists"}), 409
+        return jsonify({"message": "Username and password are required"}), 400
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({"message": "Username already exists"}), 409
     new_user = User(username=username)
-    new_user.set_password(password)
+    new_user.set_password_hash(password)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"msg": "User created"}), 201
+    return jsonify({"message": "User successfully created"}), 201
 
 @app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+def user_login():
+    credentials = request.get_json()
+    username = credentials.get('username')
+    password = credentials.get('password')
     user = User.query.filter_by(username=username).first()
-    if not user or not user.check_password(password):
-        return jsonify({"msg": "Invalid username or password"}), 401
-    access_token = create_access_token(identity=username)
-    refresh_token = create_refresh_token(identity=username)
-    return jsonify(access_token=access_token, refresh_token=refresh_token), 200
+    if not user or not user.verify_password(password):
+        return jsonify({"message": "Invalid username or password"}), 401
+    user_access_token = create_access_token(identity=username)
+    user_refresh_token = create_refresh_token(identity=username)
+    return jsonify(access_token=user_access_token, refresh_token=user_refresh_token), 200
 
 @app.route('/protected', methods=['GET'])
 @jwt_required()
-def protected():
+def access_protected_resource():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
